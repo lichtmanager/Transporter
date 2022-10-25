4655f0a5f574e5f533ea2f9813f70e89eeaed297
Philipp Lichtmannegger <phliit00@hs-esslingen.de>
2022-10-25 21:54:52 +0200

    T4US3 finished.
    Only ToDo left is to clean up code and to check for variables and function size!

--- Truck.cs
+++ Truck.cs
@@ -5,21 +5,24 @@ public class Truck
     private int TruckType;
     private int TruckAge;
     private int TruckLocation;
-    private int Size;
-    private int Performance;
-    private int MaxPayload;
-    private int Consumption;
+    private int TruckSize;
+    private int TruckPerformance;
+    private int TruckMaxPayload;
+    private int TruckConsumption;
+    private double TruckPrice;
 
-    private Truck(int truckType, int truckAge, int truckLocation, int truckSize, int performance, int maxPayload,
-        int consumption)
+    private Truck(int truckType, int truckAge, int truckLocation, int truckSize, int truckPerformance,
+        int truckMaxPayload,
+        int truckConsumption, double truckPrice)
     {
         this.TruckType = truckType;
         this.TruckAge = truckAge;
         this.TruckLocation = truckLocation;
-        this.Size = truckSize;
-        this.Performance = performance;
-        this.MaxPayload = maxPayload;
-        this.Consumption = consumption;
+        this.TruckSize = truckSize;
+        this.TruckPerformance = truckPerformance;
+        this.TruckMaxPayload = truckMaxPayload;
+        this.TruckConsumption = truckConsumption;
+        this.TruckPrice = truckPrice;
     }
 
     public static void InitializeNewTrucks(int numberOfTrucksToCreate)
@@ -34,29 +37,90 @@ public class Truck
             int perf = GenerateTruckPerformance(size);
             int payload = GenerateMaxPayload(size, type);
             int cons = GenerateTruckConsumption(type, size, age);
+            double price = GenerateTruckPrice(age, size, type);
 
             Truck truck = new Truck(
-                type, age, loc, size, perf, payload, cons);
+                type, age, loc, size, perf, payload, cons, price);
             listOfTrucks.Add(truck);
         }
 
-        //prints generated trucks
+
         for (int i = 0; i < numberOfTrucksToCreate; i++)
         {
             Console.WriteLine(
-                    $"{i + 1}: {MappedTruckType(listOfTrucks[i].TruckType)}   \t {MappedTruckAge(listOfTrucks[i].TruckAge)}\t Standort: {MappedTruckLocation(listOfTrucks[i].TruckLocation)} " +
-                    $" \t Größe: {MappedTruckSize(listOfTrucks[i].Size)} \t Perf: {listOfTrucks[i].Performance.ToString()} " +
-                    $" \t max. Load: {listOfTrucks[i].MaxPayload.ToString()} \t Cons:{listOfTrucks[i].Consumption.ToString()}");
-            
-            
-        } 
+                $"{i + 1}: {MappedTruckType(listOfTrucks[i].TruckType)}   \t {MappedTruckAge(listOfTrucks[i].TruckAge)}\t Standort: {MappedTruckLocation(listOfTrucks[i].TruckLocation)} " +
+                $" \t Größe: {MappedTruckSize(listOfTrucks[i].TruckSize)} \t Perf: {listOfTrucks[i].TruckPerformance.ToString()} " +
+                $" \t max. Load: {listOfTrucks[i].TruckMaxPayload.ToString()} \t Cons:{listOfTrucks[i].TruckConsumption.ToString()} \t Preis: {listOfTrucks[i].TruckPrice.ToString("0")}€");
+        }
+    }
+
+    private static double GenerateTruckPrice(int age, int size, int type)
+    {
+        double priceFactor;
+        int basePrice;
+        double price;
+
+        Random rnd = new Random();
+
+        switch (size)
+        {
+            case 0:
+                basePrice = 25000;
+                priceFactor = (1 + rnd.Next(-20, 31) / 100.0) - age * 0.03;
+                price = basePrice * priceFactor;
+                if (type == 0)
+                {
+                    return price + 0.1;
+                }
+                else
+                {
+                    return price;
+                }
+            case 1:
+                basePrice = 60000;
+                priceFactor = (1 + rnd.Next(-20, 31) / 100.0) - age * 0.03;
+                price = basePrice * priceFactor;
+                if (type == 0)
+                {
+                    return price + 0.1;
+                }
+                else
+                {
+                    return price;
+                }
+            case 2:
+                basePrice = 80000;
+                priceFactor = (1 + rnd.Next(-20, 31) / 100.0) - age * 0.03;
+                price = basePrice * priceFactor;
+                if (type == 0)
+                {
+                    return price + 0.1;
+                }
+                else
+                {
+                    return price;
+                }
+            case 3:
+                basePrice = 120000;
+                priceFactor = (1 + rnd.Next(-20, 31) / 100.0) - age * 0.03;
+                price = basePrice * priceFactor;
+                if (type == 0)
+                {
+                    return price + 0.1;
+                }
+                else
+                {
+                    return price;
+                }
+            default: return price = 0;
+        }
     }
 
     private static int GenerateTruckType()
     {
         Random rnd = new Random();
         int truckType = rnd.Next(0, 3);
-      //  Console.WriteLine($"Typengereator: {truckType}");
+
         return truckType;
     }
 
@@ -80,7 +144,6 @@ public class Truck
     {
         Random rnd = new Random();
         int size = rnd.Next(0, 4);
-        Console.WriteLine($"Größengenerator: {size}");
 
         return size;
     }
@@ -93,19 +156,19 @@ public class Truck
         {
             case 0:
                 perf = rnd.Next(10, 26);
-                //Console.WriteLine("perf. klein");
+
                 return perf;
             case 1:
                 perf = rnd.Next(30, 51);
-               // Console.WriteLine("perf. medium");
+
                 return perf;
             case 2:
                 perf = rnd.Next(40, 71);
-               // Console.WriteLine("perf. groß");
+
                 return perf;
             case 3:
                 perf = rnd.Next(60, 81);
-               // Console.WriteLine("perf. rießig");
+
                 return perf;
             default:
                 return perf = 0;
@@ -153,21 +216,7 @@ public class Truck
     private static int GenerateTruckConsumption(int type, int size, int age)
     {
         var consumption = 0;
-        switch (age)
-        {
-            case <= 2:
-                consumption += 1;
-                break;
-            case var n and >= 3 and <= 5:
-                consumption += 2;
-                break;
-            case var n and >= 6 and <= 8:
-                consumption += 3;
-                break;
-            case > 9:
-                consumption += 4;
-                break;
-        }
+        consumption += (age / 3);
 
         switch (size)
         {
