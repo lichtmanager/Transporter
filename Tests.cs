a7d4967b1b827a01044629924b84bbe82bf4a1d3
Philipp Lichtmannegger <info@lichtmannegger>
2022-11-11 08:35:26 +0100

    added class Tests.cs

---
+++ Tests.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace Abgabe_1_2;
+
+public class Tests
+{
+    public void Test_that_1_plus_1_equals_2()
+    {
+        var actual = 1 + 1;
+        Assert.Equal(2, actual);
+    }
+}
